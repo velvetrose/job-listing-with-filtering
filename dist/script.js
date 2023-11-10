909bef3da3e92da58fde94d0c387bb1cba35030f
Sarah Susan <55784685+velvetrose@users.noreply.github.com>
2023-11-10 13:06:25 +0530

    coode updated

--- dist/script.js
+++ dist/script.js
@@ -5,16 +5,7 @@ const data = fetch("/data.json")
   .then((data) => getData(data));
 
 function getData(data) {
-  //  data.filter((eventData)=>{
-  //     if(chipArray.length === 0){
-  //       return eventData;
-  //     }
-  //     else if(eventData.languages.includes(chipArray.values)){
-  //       return eventData;
-  //     }
-  //   })
-  // console.log(data.forEach((entry)=>{console.log(entry.languages.includes(chipArray.values))}));
-  // console.log(data.forEach((entry)=>{console.log(entry.languages.some(r=> chipArray.includes(r)))}));
+
   data.forEach((item) => {
     let twClass = item.featured
       ? " border-l-4 border-l-cyan-desaturated-dark"
@@ -33,7 +24,7 @@ function getData(data) {
           ${checkTrue(item)}
           </div>
         </div>
-        <div class="item-role">${item.position}</div>
+        <div class="item-role" onclick="filterRole('${item.role}')">${item.position}</div>
         <ul class="item-desc">
           <li>${item.postedAt}</li>
           <li>${item.contract}</li>
@@ -50,34 +41,7 @@ function getData(data) {
   </div>`
     );
 
-    // let tagSelect = document.querySelectorAll(".tag");
-    // let chipSection = document.querySelector(".filter-chips");
-    // let chipArray = [];
-
-    // for (let i = 0; i < tagSelect.length; i++) {
-    // tagSelect[i].onclick = () => {
-    // chipSection.classList.remove("invisible");
-    // if (!chipArray.includes(tagSelect[i].innerText)) {
-    //   chipArray.push(tagSelect[i].innerText);
-    //   chipSection.insertAdjacentHTML(
-    //     "beforeend",
-    //     `<div class="filter-tag">
-    //     <p class="filter-label">${tagSelect[i].innerText}</p>
-    //     <img src="images/icon-remove.svg" alt="close-icon" class="filter-close" onclick=closeFilter(event)>
-    //     </div>`
-    //   );
-    // }
-    // };
-    // }
   });
-
-  // let itemSelect = document.querySelectorAll('.item');
-  // itemSelect.forEach((value)=>{
-  //   let featuredText = value.childNodes[1].childNodes[3].childNodes[1].childNodes[3].childNodes[3];
-  //   if(featuredText == undefined){
-  //     value.style.border='none';
-  //   }
-  // })
 }
 function child(param) {
   let str = "";
@@ -96,6 +60,46 @@ function checkTrue(a) {
 
 let chipArray = [];
 
+function filterRole(role) {
+  console.log(role);
+  let chipSection = document.querySelector(".filter-chips");
+  chipSection.classList.remove("invisible");
+
+  if (!chipArray.includes(role)) {
+    chipArray.push(role);
+    // console.log(chipArray);
+    chipSection.insertAdjacentHTML(
+      "beforeend",
+      `<div class="filter-tag">
+      <p class="filter-label">${role}</p>
+      <img src="images/icon-remove.svg" alt="close-icon" class="filter-close" onclick=closeFilter(event)>
+      </div>`
+    );
+  }
+
+  const data = fetch("/data.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let filteredData = data.filter((x) =>
+        chipArray.every(
+          (i) =>
+            x.role.includes(i) ||
+            x.level.includes(i) ||
+            x.languages.includes(i) ||
+            x.tools.includes(i)
+        )
+      );
+      let container = document.querySelector(".main-container");
+      let firstItem = container.firstElementChild;
+      //using for loop
+      for (i = 0; i < container.children.length; ) {
+        container.children[0].remove();
+      }
+      getData(filteredData);
+    });
+}
 function addFilter(e) {
   let chipSection = document.querySelector(".filter-chips");
   chipSection.classList.remove("invisible");
@@ -112,26 +116,34 @@ function addFilter(e) {
     );
   }
 
-  console.log(chipArray);
+  console.log("Chip Values ", chipArray);
 
-
-    // const data = fetch("/data.json")
-    // .then((response) => {
-    //   return response.json();
-    // })
-    // .then((data) => {
-    //   // let filteredData = data.filter((x) => x.role === "Frontend");
-    //   let filteredData = data.filter((x) => x.languages.includes('HTML'));
-    //   console.log(filteredData);
-
-    //   let container = document.querySelector(".main-container");
-    //   let firstItem = container.firstElementChild;
-    //   //using for loop
-    //   for (i = 0; i < container.children.length;) {
-    //     container.children[0].remove();
-    //   }
-    //   getData(filteredData);
-    // });
+  const data = fetch("/data.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // let filteredData = data.filter((x) => x.role === "Frontend");
+      // let filteredData = data.filter((x) => x.languages.includes('HTML'));
+      let filteredData = data.filter((x) =>
+        chipArray.every(
+          (i) =>
+            x.role.includes(i) ||
+            x.level.includes(i) ||
+            x.languages.includes(i) ||
+            x.tools.includes(i)
+        )
+      );
+      console.log("Active Chips ", filteredData);
+      // filteredData.sort((a, b) => a.id - b.id);
+      let container = document.querySelector(".main-container");
+      let firstItem = container.firstElementChild;
+      //using for loop
+      for (i = 0; i < container.children.length; ) {
+        container.children[0].remove();
+      }
+      getData(filteredData);
+    });
 }
 
 function closeFilter(e) {
@@ -144,13 +156,30 @@ function closeFilter(e) {
     chipSection.classList.add("invisible");
   }
 
-
+  console.log(chipArray.length);
 
   const data = fetch("/data.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    getData(data);
-  });
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+
+      let filteredData = data.filter((x) =>
+        chipArray.every(
+          (i) =>
+            x.role.includes(i) ||
+            x.level.includes(i) ||
+            x.languages.includes(i) ||
+            x.tools.includes(i)
+        )
+      );
+      console.log("Active Chips after Closing ", filteredData);
+      let container = document.querySelector(".main-container");
+      let firstItem = container.firstElementChild;
+      //using for loop
+      for (i = 0; i < container.children.length; ) {
+        container.children[0].remove();
+      }
+      getData(filteredData);
+    });
 }
